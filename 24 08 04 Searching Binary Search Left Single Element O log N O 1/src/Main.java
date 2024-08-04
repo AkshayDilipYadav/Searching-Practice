3cf88f502b18e90cc36109a2f83fef1fedac9f0b
Ace <152076508+AkshayDilipYadav@users.noreply.github.com>
2024-08-04 21:14:42 +0530

    Add files via upload

---
+++ 24 08 04 Searching Binary Search Left Single Element O log N O 1/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] A = new int[n];
+
+        for(int i = 0 ; i < n; i++){A[i] = sc.nextInt();}
+
+
+        int result = solve(A);
+
+        System.out.print(result);
+
+
+
+    }
+    public static int solve(int[] A) {
+        int left = 0;
+        int right = A.length - 1;
+        while(left < right){
+            int mid = left + (right - left) / 2;
+            if(mid % 2 == 1){mid--;}
+            if(A[mid] == A[mid + 1]){left = mid + 2;}
+            else{right = mid;}
+
+        }
+        return A[left];
+    }
+
+}
